6edfddffb253d127bbc7cff84e5b9e941268d036
moronifacundo <moronifacundo@gmail.com>
2022-04-25 18:09:13 -0300

    [UPDATED] utils/fetcher + peoplesection - PRETTIEFIED

--- arturito/src/utils/fetcher.ts
+++ arturito/src/utils/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Possible alternative
-const baseURL = 'https://swapi.dev/api'
+const baseURL = 'https://swapi.dev/api';
 // const baseURL = 'https://www.swapi.it/api';
 
 export const swGet = (url: string) =>
