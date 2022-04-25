3a6da21443bf142ba4902e6b951ed7ecd9a0ec40
moronifacundo <moronifacundo@gmail.com>
2022-04-25 17:44:48 -0300

    [UPDATE] Main/index - render the people component

--- arturito/src/components/PeopleSection/index.tsx
+++ arturito/src/components/PeopleSection/index.tsx
@@ -6,39 +6,44 @@ import { swGet } from '../../utils/fetcher';
 import Table from '../Table';
 
 const columns = [
-  {
-    title: 'Birth Year',
-    dataIndex: 'birth_year',
-    key: 'birth_year',
-  },
-  {
-    title: 'Height',
-    dataIndex: 'height',
-    key: 'height',
-  },
-  {
-    title: 'Films count',
-    dataIndex: 'films',
-    key: 'films_count',
-    render: (films: string[]) => films.length,
-  },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Birth Year',
+        dataIndex: 'birth_year',
+        key: 'birth_year',
+    },
+    {
+        title: 'Height',
+        dataIndex: 'height',
+        key: 'height',
+    },
+    {
+        title: 'Films count',
+        dataIndex: 'films',
+        key: 'films_count',
+        render: (films: string[]) => films.length,
+    },
 ];
 
 const People = () => {
-  const { data, error } = useSWR('/people', swGet);
+    const { data, error } = useSWR('/people', swGet);
 
-  if (error) {
-    return <div className="px-2">Oh oh!</div>;
-  }
-  if (!data) {
-    return <div className="px-2">Loading...</div>;
-  }
+    if (error) {
+        return <div className="px-2">Oh oh!</div>;
+    }
+    if (!data) {
+        return <div className="px-2">Loading...</div>;
+    }
 
-  return (
-    <div>
-      <Table columns={columns} data={data.results} /* :D */ />
-    </div>
-  );
+    return (
+        <div>
+            <Table columns={columns} data={data.results} /* :D */ />
+        </div>
+    );
 };
 
 export default People;
